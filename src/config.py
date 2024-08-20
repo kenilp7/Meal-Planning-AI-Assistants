d4a19b1861fd33ba0acbbc5b5ad698079583406c
kenilp7 <kenilpatel2047@gmail.com>
2024-08-20 12:01:26 +0200

    update config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 from getpass import getpass
 
-# Set up API key for GROQ
+# Setting up API key for GROQ
 tokenGROQ = getpass('Enter GROQ_API_KEY here: ')
 os.environ["GROQ_API_KEY"] = tokenGROQ
