6a64b7bf60814c74bb17bc854fec400d4ca4c450
kenilp7 <kenilpatel2047@gmail.com>
2024-08-23 12:10:06 +0200

    update team

--- src/team.py
+++ src/team.py
@@ -2,7 +2,7 @@ from autogen_agentchat.conditions import MaxMessageTermination
 from autogen_agentchat.teams import RoundRobinGroupChat
 from .agents import budget_agent, breakfast_agent, lunch_agent, dinner_agent, snack_agent
 
-termination = MaxMessageTermination(max_messages=10)
+termination = MaxMessageTermination(max_messages=8)
 
 team = RoundRobinGroupChat(
     participants = [budget_agent, breakfast_agent, lunch_agent, dinner_agent, snack_agent],
